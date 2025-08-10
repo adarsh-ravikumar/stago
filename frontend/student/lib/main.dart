6ea61ce59f02f5b0d663ddb07313418ed5f00d24
Yashu Maurya <yashuff1@gmail.com>
2025-08-10 20:12:35 +0530

    chore: added splash screen for initial logged in checkup

--- frontend/student/lib/main.dart
+++ frontend/student/lib/main.dart
@@ -1,20 +1,62 @@
 import 'package:flutter/material.dart';
+import 'package:student_frontend/Environment/supabase.dart'; // Added to .gitignore, use your own >:(
+import 'package:student_frontend/Views/home_page.dart';
 import 'package:student_frontend/Views/login_page.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
-  runApp(const MyApp());
+Future<void> main() async {
+  await Supabase.initialize(url: supabase_url, anonKey: supabase_anon_key);
+  runApp(MyApp());
 }
+
+// Global Supabase Variable
+final supabase = Supabase.instance.client;
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(colorScheme: ColorScheme.dark()),
-      home: LoginPage(),
+      home: SplashPage(context: context),
     );
+  }
+}
+
+// Splash Page to decide which page to show on startup.
+class SplashPage extends StatefulWidget {
+  final BuildContext context;
+  const SplashPage({super.key, required this.context});
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    super.initState();
+    _redirect();
+  }
+
+  Future<void> _redirect() async {
+    final session = supabase.auth.currentSession;
+
+    if (!mounted) return;
+
+    if (session != null) {
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (context) => const HomePage()),
+      );
+    } else {
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (context) => const LoginPage()),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(body: Center(child: CircularProgressIndicator()));
   }
 }
